5f40868fcb407ae2e1778fd54660a065a9078856
GemmaTuron <gemma@ersilia.io>
2025-04-28 20:22:50 +0200

    update workflows

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM bentoml/model-server:0.11.0-py38
 MAINTAINER ersilia
 
-RUN pip install rdkit
+RUN pip install rdkit==2024.3.5
 RUN pip install numpy==1.23.1
 RUN pip install fastai==1.0.61
-RUN pip install scikit-learn
-RUN pip install spacy
+RUN pip install scikit-learn==1.3.2
+RUN pip install spacy==3.0.0
 
 
 WORKDIR /repo
--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -92,7 +92,7 @@ assert input_len == output_len
 # write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["dimension_{0}".format(str(i).zfill(3)) for i in range(400)])  # header
+    writer.writerow(["dim_{0}".format(str(i).zfill(3)) for i in range(400)])  # header
     for o in outputs:
         writer.writerow(list(o))
 
